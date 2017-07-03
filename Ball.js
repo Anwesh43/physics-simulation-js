d8f87b5525e2f555ffea6da142ae5a88c9ed37ec
Anwesh43 <anweshthecool0@gmail.com>
2017-07-04 03:40:01 +0530

    adding drawing fixes

--- Ball.js
+++ Ball.js
@@ -1,6 +1,6 @@
 class PhysicsBody {
     constructor(centerx,centery,radius,color) {
-        this.position = new Vector(centerx,cenetery)
+        this.position = new Vector(centerx,centery)
         this.radius = radius
         this.color = color
     }
@@ -8,7 +8,7 @@ class PhysicsBody {
         this.vel = velvec
     }
     applyForce(force) {
-        this.acc = force/1
+        this.acc = force
     }
     draw(context) {
         context.fillStyle = this.color
@@ -18,6 +18,7 @@ class PhysicsBody {
         context.arc(0,0,this.radius,0,2*Math.PI)
         context.fill()
         context.restore()
+        console.log(this.position)
     }
     update() {
         this.position.add(this.vel)
